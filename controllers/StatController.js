a3b7044ef7d5eeba614f1b6b3da4e8629b10895d
Diana Baburina <diana.baburina@code-care.pro>
2019-04-08 12:53:44 +0300

    add sectors statistics for device

--- controllers/StatController.js
+++ controllers/StatController.js
@@ -16,7 +16,19 @@ const StatController = {
       throw new errors.ServerError(`Error in getting service stat for device, error: ${error.message}`);
     }
   },
-  getSectorsStat: () => {},
+  getSectorsStat: async (ctx) => {
+    const { deviceId } = ctx.params;
+
+    const device = await DeviceProvider.checkIfExists(deviceId);
+    if (!device) throw new errors.ClientError(`No device with such id: ${deviceId}`);
+
+    try {
+      const sectorsStat = await StatProvider.getSectorsStat(deviceId);
+      return ctx.send(200, sectorsStat[0]);
+    } catch (error) {
+      throw new errors.ServerError(`Error in getting sectors stat for device, error: ${error.message}`);
+    }
+  },
 };
 
 module.exports = StatController;
--- providers/StatProvider.js
+++ providers/StatProvider.js
@@ -6,6 +6,14 @@ const StatProvider = {
             from "Services" s
             join "Devices" d on d.id = s."deviceId"
             where s."deviceId" = '${deviceId}'`),
+  getSectorsStat: deviceId => sequelize
+    .query(`select st.id, st.uuid, st.name, st."maxTemperature" as "maxAccessTemp", 
+            st."minTemperature" as "minAccessTemp", st."trackerSetupDate",
+            s."currentTemp", s."criticalCount", s."avgTemperature", s."maxTemperature", 
+            s."minTemperature", s."timeExcess" as "lastTimeExcess"
+            from "SectorTrackers" st
+            join "TrackerStatuses" s on s."sectorId" = st.id
+            where st."deviceId" = '${deviceId}'`),
 };
 
 module.exports = StatProvider;
