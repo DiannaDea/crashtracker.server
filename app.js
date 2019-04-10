36192c62b44355e817e5b02989806dd18803678e
Diana Baburina <diana.baburina@code-care.pro>
2019-04-10 22:07:12 +0300

    add koa-cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Koa = require('koa');
 const respond = require('koa-respond');
 const logger = require('koa-logger');
+const cors = require('@koa/cors');
 
 const { routes } = require('./routes');
 const errorHandler = require('./utils/errorHandler.js');
@@ -10,6 +11,7 @@ const app = new Koa();
 require('./services/postgresConnection');
 
 app.use(errorHandler);
+app.use(cors());
 app.use(respond());
 app.use(logger());
 
